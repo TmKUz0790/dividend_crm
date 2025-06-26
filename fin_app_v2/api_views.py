2d9622c423e0c5e5e86bb70a008a4978d6340277
Akbar Ilkhambaev <akbarilxambaev@gmail.com>
2025-06-27 01:49:28 +0500

    фффывфы

--- fin_app_v2/api_views.py
+++ fin_app_v2/api_views.py
@@ -312,11 +312,14 @@ def calendar_tasks(request):
         })
 
     return Response(tasks_by_date)
+
+
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions, parsers
 from django.shortcuts import get_object_or_404
-from .models import CrmJob, CrmTask, CrmTaskFile, CrmTaskComment
+from .models_crm import CrmJob, CrmTask, CrmTaskFile, CrmTaskComment
 from .serializers import CrmTaskSerializer, CrmTaskFileSerializer, CrmTaskCommentSerializer
 import json
 
